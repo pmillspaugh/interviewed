6ae231f88b55e9efc8d1e8422b8789cb237f9f91
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-06-08 17:12:21 -0400

    create UI theme file

---
+++ client/src/theme.js
@@ -0,0 +1,22 @@
+// Color palette generated on coolors.co --> https://coolors.co/3d3522-4a442d-e4dfce-386150-58b09c-caf7e2-1a47cb-73aeed-e0f1ff
+const colors = {
+  darkBrown: 'hsl(42 28% 19%)',
+  mediumBrown: 'hsl(48 24% 23%)',
+  lightBrown: 'hsl(46 29% 85%)',
+  darkGreen: 'hsl(155 27% 30%)',
+  mediumGreen: 'hsl(166 36% 52%)',
+  lightGreen: 'hsl(152 74% 88%)',
+  darkBlue: 'hsl(225 77% 45%)',
+  mediumBlue: 'hsl(211 77% 69%)',
+  lightBlue: 'hsl(207 100% 94%)',
+};
+
+const lightTheme = {
+  backgroundPrimary: colors.lightBlue,
+};
+
+const darkTheme = {
+  backgroundPrimary: colors.darkBrown,
+};
+
+export { lightTheme, darkTheme };
