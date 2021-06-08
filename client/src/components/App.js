5f5db2857416483dccc6d05b356b6b58a641aa50
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-06-08 10:47:10 -0400

    set up barebones React frontend file structure

---
+++ client/src/components/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <main>
+      <h1>App component placeholder</h1>
+      <p>This is the App component boilerplate</p>
+    </main>
+  );
+};
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+// import Redux store and Provider
+// import global styles
+// add analytics?
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+);
